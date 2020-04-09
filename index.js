10a3e5f57bbdddc807f74a4b5a40e288ae8400d0
sniperdevil <muge111hhh@gmail.com>
2020-04-09 15:13:16 +0530

    auth handle

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express'),
-mongoose = require('mongoose'),
+mongoose = require('mongoose');
+const passport = require('passport');
+//passport setuo 
+require('./config/passport')(passport);
 app = express();
 
 const port = process.env.PORT || 5000;
+
+
 
 app.get('/', (re, res) => {
     res.send('hii from node.js');
 })
 
+app.use('/auth', require('./routes/auth'));
 
 app.listen(port, ()=> {
     console.log(`server running of ${port}`);
